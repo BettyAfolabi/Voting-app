8b0e90a0f6ce8a61dca90f688e0ce95e059f583a
Elizabeth <betty4web@gmail.com>
2023-03-05 10:58:32 +0100

    updating TextInput

--- src/Screen/register.jsx
+++ src/Screen/register.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 
 
 const Register = () => {
-  const [value, setValue] = React.useState('');
+  const [value, setValue] = React.useState('')
   
   return(
     <div className=' w-full h-screen flex flex-col px-5 pt-12 box-border font-sans md:w-80 md:h-full md:my-3 md:mx-auto md:bg-green-50 md:rounded-3xl '>
@@ -18,7 +18,7 @@ const Register = () => {
           <option value="social">Social Security number</option>
           <option value="license">Driver's license</option>
    		  </select>
-        <TextInput value={value} handleChange={(e) => setValue(e)} placeholder='Enter ID No.' required />
+        <TextInput value={value} handleChange={(e) => setValue(e)} placeholder='Enter ID No.' />
         <Link to={'/Verify-identity'} className='w-full mt-60 md:mb-3' >
             <Button label="Proceed" handleClick={() => {}} />
         </Link>
--- src/components/textinput.jsx
+++ src/components/textinput.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import { useState } from "react";
 
 const TextInput = ({ value, placeholder = 'Enter Text', handleChange }) => {
+    const [required, setRequired] = useState({ required: true});
     return(
-        <input onChange={(e) => handleChange(e.target.value)} value={value} placeholder={placeholder} className='w-full h-12 rounded-md border-1 border-gray-200 px-2 mb-4 bg-gray-100' />
+        <input {...required} onChange={(e) => handleChange(e.target.value)} value={value} placeholder={placeholder} className='w-full h-12 rounded-md border-1 border-gray-200 px-2 mb-4 bg-gray-100' />
+      
     )
 }
 
